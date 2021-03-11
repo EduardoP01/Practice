ac4d7024a6120d1ee22e2d070f915fb37820e73b
unknown <eduardo.p.2106@aim.edu.mx>
2021-03-10 18:48:40 -0600

    Actualizacion de Semana 2

--- semana2.js
+++ semana2.js
@@ -50,3 +50,69 @@ switch (condicionalSwitch){
         console.log("El valor ingresado es distinto");
         break;
 }
+
+console.log("Sesion 6 ------------------------------")
+
+//Para Ingresar datos:
+//En terminal: npm install prompt-sync
+//const prompt = require('prompt-sync')();
+//var nombre = prompt ("Ingresa tu nombre");
+//console.log("Hola " + nombre);
+
+//Ejercicio (Al ingresar la hora te dice que pasa en la cancha)
+
+const prompt = require('prompt-sync')();
+//var horaDia = prompt("Que horas son? ");
+//console.log(horaDia);
+/* Borrar esta para el codigo del ejercicio
+
+if (horaDia <= 3){
+    console.log("Esta el equipo de Soccer entrenando");
+}
+else if ( (horaDia > 3) && (horaDia < 6) ) {
+    console.log("Esta el equipo de Futbol entrenando");
+}
+else if ((horaDia > 6) && (horaDia <= 9)) {
+    console.log("Esta el equipo de Atletismo entrenando");
+}
+
+
+//Ejercicio con Switch
+/*
+switch(horaDia <= 3){
+    case true:
+        console.log("Esta el equipo de Soccer entrenando");
+        break;
+    case false:
+        console.log("Esta otro equipo entrenando");
+        break;
+}
+*/
+
+//Ejercicio inverso (Dado el deporte te da la hora)
+/*
+var deporte = prompt("Que deporte esta siendo jugado? ");
+switch (deporte){
+    case "Soccer":
+        console.log ("Horario: 1 - 3 pm");
+        break;
+    case "Futbol":
+        console.log ("Horario: 3 - 6 pm");
+        break;
+    case "Atletismo":
+        console.log("Horario: 6 - 9 pm");
+        break;
+}
+*/
+
+//Arreglos
+var arregloNumerico = [1, 2, 3, 4, 5];
+console.log (arregloNumerico);
+
+var arregloTexto = ["Hola", "Eduardo", "Interesante"];
+console.log(arregloTexto);
+
+var arregloMixto = ["Hola", 20, ,"Eduardo", 10, "AIM"];
+console.log(arregloMixto);
+
+console.log( arregloTexto[0] );
